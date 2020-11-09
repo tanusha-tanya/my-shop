9a942660fdaf9547bd4a1bd6a0fd141b92f330b3
Tatyana <verevkina@picom.ru>
2020-11-10 03:04:53 +0400

    product

--- src/views/Products/Product.vue
+++ src/views/Products/Product.vue
@@ -1,14 +1,43 @@
 <template>
   <v-container>
-    <v-layout row>
-      <v-flex xs12>
-        <h1>Product</h1>
-      </v-flex>
-    </v-layout>
+    <section class="product mt-3 elevation-10">
+      <v-layout row wrap>
+        <v-flex xs12 lg6>
+          <v-img src="https://image.ibb.co/cpScgo/ASUS_FX503_VD.jpg" class="product_img"></v-img>
+        </v-flex>
+        <v-flex xs12 lg6>
+          <div class="product_info">
+            <h5 class="product_title display-1 mb-3 mt-3">Lorem lorem</h5>
+            <p class="product_category title">
+              <span class="product_title">Vendor: </span> Lorem
+            </p>
+            <p class="product_price title">
+              <span class="product_title">Price: </span>$ 1255
+            </p>
+            <p class="product_color title">
+              <span class="product_title">Color: </span>
+              <span
+                :title="'purple'"
+                :style = "{backgroundColor:'purple'}"
+                class="product_color__bg"
+              ></span>
+            </p>
+            <p class="product_material title">
+              <span class="product_title">Material: </span>plastic
+            </p>
+            <div class="title mb-5">
+              <p class="product_title mb-2">Description: </p> Intel Core i5 7300HQ 2500 MHz/15.6"/1920x1080/8Gb/1000Gb HDD/DVD нет/NVIDIA GeForce GTX 1050, 4096 МБ/Wi-Fi/Bluetooth/Win 10 Home
+            </div>
+            <v-btn color="primary" class="headline mr-2">Edit</v-btn>
+            <v-btn color="primary" class="headline">Buy</v-btn>
+          </div>
+        </v-flex>
+      </v-layout>
+    </section>
   </v-container>
 </template>
 <script>
-  export default {
+  export default { 
     data () {
       return {
         
@@ -16,3 +45,36 @@
     }
   }
 </script>
+<style scoped>
+  .product {
+    display: flex;
+    align-items: center;
+    flex-wrap: wrap;
+    border: 1px solid #1c77ad;
+    padding: 30px;
+    margin-bottom: 100px;
+  }
+  .product_img {
+    height: 100%;
+    width: 550px;
+  }
+  .product_info {
+    margin-left: 50px;
+  }
+  .product_title {
+    font-size: 1.6rem;
+    font-weight: bold;
+    color: #1875d0;
+    margin-bottom: 0;
+  }
+  .product_price {
+    color: red
+  }
+  .product_color__bg {
+    display: inline-block;
+    width: 50px;
+    height: 20px;
+    border: 1px solid #2b2b2b;
+    border-radius: 10px;
+  }
+</style>
--- src/views/Products/ProductList.vue
+++ src/views/Products/ProductList.vue
@@ -1,8 +1,32 @@
 <template>
   <v-container>
     <v-layout row>
-      <v-flex xs12>
-        <h1>My Products</h1>
+      <v-flex xs12 sm6 offset-sm3>
+        <h1 class="text--secondary mb3">My Products</h1>
+        <v-card 
+          class="elevation-10 mb-2" 
+          v-for="(product, i) in products"
+          :key="i">
+          <v-layout>
+            <v-flex xs4>
+              <v-img
+                :src="product.imageSrc"
+                height="250px">
+              </v-img>
+            </v-flex>
+            <v-flex xs8>
+              <v-card-text>
+                <h2 class="text--primary">{{product.title}}</h2>
+                <p>{{product.description}}</p>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn class="info"
+                  :to="'/product/'+ product.id">Open</v-btn>
+              </v-card-actions>
+            </v-flex>
+          </v-layout>
+        </v-card>
       </v-flex>
     </v-layout>
   </v-container>
@@ -11,7 +35,30 @@
   export default {
     data () {
       return {
-        
+        products: [
+          {
+            id: '1',
+            title: 'Lenovo Legion Y520',
+            vendor: 'Lenovo',
+            color: 'black',
+            material: 'metal/plastic',
+            description: 'Intel Core i5 7300HQ 2500 MHz/15.6"/1920x1080/8Gb/1000Gb HDD/DVD нет/NVIDIA GeForce GTX 1050, 4096 МБ/Wi-Fi/Bluetooth/Win 10 Home',
+            price: 760,
+            promo: false,
+            imageSrc: 'https://image.ibb.co/fZzq1o/Lenovo_Legion_Y520.jpg',
+          },
+          {
+            id: '2',
+            title: 'Asus FX503VD',
+            vendor: 'Asus',
+            color: 'white',
+            material: 'plastic',
+            description: 'Intel Core i5 7300HQ 2500 MHz/15.6"/1920x1080/8Gb/256Gb SSD/DVD нет/NVIDIA GeForce GTX 1050/Wi-Fi/Bluetooth/Windows 10 Home',
+            price: 984,
+            promo: true,
+            imageSrc: 'https://image.ibb.co/cpScgo/ASUS_FX503_VD.jpg',
+          }
+        ]
       }
     }
   }
